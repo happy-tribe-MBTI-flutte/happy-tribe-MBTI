81c1f417ae5a09af5b71c3171a5b8b227dc420da
joyoung <githuborigin@gmail.com>
2023-10-04 16:25:01 +0900

    메인 페이지 결과페이지 수정

--- mbti/lib/main.dart
+++ mbti/lib/main.dart
@@ -19,47 +19,49 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-
   var mbtiStr = "intp";
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage('page_img/m_img.png') ,fit:BoxFit.cover )),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                '나와 맞는 여행지에 어울리는 술과 노래는 뭘까?',
-                style: TextStyle(
-                    letterSpacing: 1,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w700),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text('흥의 민족',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-              SizedBox(
-                height: 20,
-              ),
-              Text('MBTI',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold , color: Colors.blue)),
-              SizedBox(
-                height: 40,
-              ),
-
-              SizedBox(
-                width: 250,
-                height: 50,
-                child: ElevatedButton(
-                    style: ButtonStyle(
-                      padding: MaterialStatePropertyAll(
-                        EdgeInsets.all(20),
-                      ),
-                    ),
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('page_img/m_img.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '나와 맞는 여행지에 어울리는 술과 노래는 뭘까?',
+                  style: TextStyle(
+                      letterSpacing: 1,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('흥의 민족',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('MBTI',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue)),
+                SizedBox(
+                  height: 40,
+                ),
+    
+                SizedBox(
+                  height: 50,
+                  width: 250,
+                  child: ElevatedButton(
+                    // style: ElevatedButton.styleFrom(maximumSize: Size(100, 100)),
                     onPressed: () {
                       Navigator.push(
                         context,
@@ -67,22 +69,25 @@ class _MyAppState extends State<MyApp> {
                             builder: (context) => MBTIQuestionPage()),
                       );
                     },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '테스트 하러가기',
-                          style: TextStyle(fontSize: 20 , fontWeight: FontWeight.w400),
-                        ),
-                       
-                        Icon(Icons.arrow_right),
-                      ],
-                    )),
-              ),
-              Container(height: 300 ,child: result(selectMbti :mbtiStr ))
-              // Expanded(child: MBTIQuestionPage()),
-            ]),
-      ),
-    );
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '테스트 하러가기',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w400),
+                          ),
+                          Icon(Icons.arrow_right),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                // Container(height: 300 ,child: result(selectMbti :mbtiStr ))
+                // Expanded(child: MBTIQuestionPage()),
+              ]),
+        ),
+      );
   }
 }
--- mbti/lib/resultMusic.dart
+++ mbti/lib/resultMusic.dart
@@ -2,26 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 void main() {
-  runApp(ResultMusic());
+  runApp(MaterialApp(home: ResultMusic()));
 }
 
 class ResultMusic extends StatelessWidget {
-  final String videoId = 'jEMe8gRBjLs';
-
+  const ResultMusic({super.key, this.mbti});
+  final mbti;
   @override
   Widget build(BuildContext context) {
+    
+    var playlist = {
+      "INFJ": "io0cVw3IxT8",
+      "INFP": "RYZMbzksrKA",
+      "INTP": "iz4xS8Ff72Y",
+      "INTJ": "zIZzfJZAkIU",
+      "ISTJ": "oH0rDHpVOlw",
+      "ISFJ": "h1w83jr6v7w",
+      "ISTP": "pzITbqeA5i4",
+      "ISFP": "WCqtFWjCtaM",
+      "ESTP": "k3uC15-GoKk",
+      "ESFP": "OUUj7aKHPpA",
+      "ESTJ": "UvHukCh4_JY",
+      "ESFJ": "MpQZMObaueI",
+      "ENFP": "w3VV16WEr9k",
+      "ENTP": "RH6H0Tb-qsU",
+      "ENFJ": "Cf5zMPAWnZo",
+      "ENTJ": "FQp8L7mpQig",
+    };
+
+    String? videoId = playlist["${mbti}"];
+
     return Scaffold(
-      body: Center(
-          child: YoutubePlayer(
-        controller: YoutubePlayerController(
-          initialVideoId: videoId,
-          flags: YoutubePlayerFlags(
-            mute: false,
-            autoPlay: true,
-          ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+              child: YoutubePlayer(
+                controller: YoutubePlayerController(
+                  initialVideoId: videoId ?? 'FQp8L7mpQig' ,
+                  flags: YoutubePlayerFlags(
+                    mute: false,
+                    autoPlay: true,
+                  ),
+                ),
+                showVideoProgressIndicator: true,
+              ),
+            ),
+            Text('***${mbti} 플레이 리스트***'),
+          ],
         ),
-        showVideoProgressIndicator: true,
-      )),
+      ),
     );
   }
 }
