2f5802267240487da11fd922d40165391f7aefe6
jeonghyun77 <heelli0801@naver.com>
2023-10-04 10:29:16 +0900

    mbti 페이지 이미지 추가 파일입니다

--- mbti/lib/resultDrink.dart
+++ mbti/lib/resultDrink.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(resultDrink());
+}
+
 class resultDrink extends StatelessWidget {
   const resultDrink({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+       image: DecorationImage(
+        fit: BoxFit.cover,
+        image: AssetImage('assests/page_img/r_img2.png'),
+        ),
+      ),
+      child: Scaffold(backgroundColor: Colors.transparent),
+    );
   }
 }
