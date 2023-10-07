1dbc0ded499069bee734b7391f5c28efdf6e188d
jeonghyun77 <heelli0801@naver.com>
2023-10-07 20:07:41 +0900

    drink.json파일을 추가했습니다

--- mbti/lib/resultDrink.dart
+++ mbti/lib/resultDrink.dart
@@ -1,22 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(resultDrink());
+  runApp(MaterialApp(home: resultDrink(),));
 }
 
 class resultDrink extends StatelessWidget {
-  const resultDrink({super.key});
+  const resultDrink({super.key, this.mbti,});
+
+ final mbti;
+ 
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-       image: DecorationImage(
-        fit: BoxFit.cover,
-        image: AssetImage('assests/page_img/r_img2.png'),
+    var mmm = 'mbti';
+    return Scaffold(
+      body: 
+      Container(
+        decoration: BoxDecoration(
+         image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('page_img/r_img2.png'),
+          ),
+
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 190,
+              
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(50, 40, 50, 0), padding: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),  
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(50, 40, 50, 0), padding: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black)
+              ),
+            ),
+          ],
         ),
       ),
-      child: Scaffold(backgroundColor: Colors.transparent),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          ();
+        },
+        label: const Text('다시 테스트 하기'),
+        backgroundColor: Colors.blue[600],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        
     );
   }
 }
