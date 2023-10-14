696ccedb390db9729583fc1b863f42f5172401da
jeonghyun77 <heelli0801@naver.com>
2023-10-14 22:51:36 +0900

    술추천페이지 디자인 변경

--- mbti/lib/resultDrink.dart
+++ mbti/lib/resultDrink.dart
@@ -14,6 +14,10 @@ class resultDrink extends StatelessWidget {
   });
   final mbti;
 
+
+ 
+
+
   @override
   Widget build(BuildContext context) {
     var selectedMBTI;
@@ -22,16 +26,13 @@ class resultDrink extends StatelessWidget {
       String data = await rootBundle.loadString('Drink_img/drink.json');
       selectedMBTI = jsonDecode(data);
       if (selectedMBTI != null) {
-        selectedMBTI = selectedMBTI['INTJ'];
+        selectedMBTI = selectedMBTI['INFP'];
         print(selectedMBTI.keys);
-
         return selectedMBTI;
       } else {
         return null;
       }
     }
-
-    
     return FutureBuilder(
       future: MbtiJsonDecode(),
       builder: (context, snapshot) {
@@ -42,121 +43,146 @@ class resultDrink extends StatelessWidget {
           }else{
           var futureDate = snapshot.data;
           return Scaffold(
-            body: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('page_img/r_img2.png'),
-                  ),
-              ),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 190,
-                  ),
+            backgroundColor: Colors.black,
+            body: Center(
+              child: Container(
+                    width: 380,
+                    height: 800,
+                    decoration: BoxDecoration(
+                      gradient: RadialGradient(
+                        colors: [
+                          Colors.white,
+                          Color.fromARGB(255, 227, 214, 105),
+                        ],
+                        center: Alignment(-0.5, 1)
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                child: Padding(
+                  padding: const EdgeInsets.all(45.0),
+                  child: Column(
+                    children: [
+                      Text('WHIT TRAVLE',
+                      style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+                      ),
+                      //술추천 1
                       SizedBox(
-                    width: 390,
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(50, 40, 50, 0), padding: EdgeInsets.only(left:10, right:10),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black), 
-                                     
-                        ),
+                        width: 390,
                         child: Container(
-                          height: 100,
-                          margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                          decoration: BoxDecoration(
-                          border: Border( left:BorderSide(color: Colors.black), right:BorderSide(color: Colors.black)),    
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Image.network(futureDate['img1'],
-                              height: 80,
+                          margin: EdgeInsets.fromLTRB(10, 40, 10, 0), padding: EdgeInsets.only(left:10, right:10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black), 
+                            ),
+                            child: Container(
+                              height: 100,
+                              margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                              decoration: BoxDecoration(
+                                border: Border( left:BorderSide(color: Colors.black), right:BorderSide(color: Colors.black)),    
                               ),
-                              Container(
-                                width: 100,
-                                child: Column(
-                                  children: [
-                                    Text("${futureDate['recommendation']}",
-                                    style: TextStyle(fontSize: 11,),
-                                    ),
-                                    Center(
-                                      child: Wrap(
-                                        children:[
-                                          Text("${futureDate['explanation1']}",
-                                        style: TextStyle(fontSize: 11,
-                                        ),
-                                        textWidthBasis: TextWidthBasis.parent
-                                        ),
-                                        ] 
-                                      ),
-                                    ),
-                                  ],
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Image.network(futureDate['img1'],
+                                height: 80,
                                 ),
-                              )
-                            ]
+                                Container(
+                                  width: 100,
+                                  child: Column(
+                                    children: [
+                                      Text("${futureDate['recommendation']}",
+                                      style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
+                                      ),
+                                      Center(
+                                        child: Wrap(
+                                          children:[
+                                            Text("${futureDate['explanation1']}",
+                                          style: TextStyle(fontSize: 11,
+                                          ),
+                                          textWidthBasis: TextWidthBasis.parent
+                                          ),
+                                          ] 
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ]
+                            ),
                           ),
                         ),
-                    ),
-                  ),
-           SizedBox(
-                    width: 390,
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(50, 40, 50, 0), padding: EdgeInsets.only(left:10, right:10),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black), 
-                                     
-                        ),
+                      ),
+                      //술추천 2
+                      SizedBox(
+                        width: 390,
                         child: Container(
-                          height: 100,
-                          margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                          decoration: BoxDecoration(
-                          border: Border( left:BorderSide(color: Colors.black), right:BorderSide(color: Colors.black)),    
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Image.network(futureDate['img2'],
-                              height: 80,
+                          margin: EdgeInsets.fromLTRB(10, 40, 10, 40), padding: EdgeInsets.only(left:10, right:10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),            
+                            ),
+                            child: Container(
+                              height: 100,
+                              margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                              decoration: BoxDecoration(
+                              border: Border( left:BorderSide(color: Colors.black), right:BorderSide(color: Colors.black)),    
                               ),
-                              Container(
-                                width: 100,
-                                child: Column(
-                                  children: [
-                                    Text("${futureDate['Other recommendations']}",
-                                    style: TextStyle(fontSize: 11,),
-                                    ),
-                                    Center(
-                                      child: Wrap(
-                                        children:[
-                                          Text("${futureDate['explanation2']}",
-                                        style: TextStyle(fontSize: 11,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Image.network(futureDate['img2'],
+                                  height: 80,
+                                  ),
+                                  Container(
+                                    width: 100,
+                                    child: Column(
+                                      children: [
+                                        Text("${futureDate['Other recommendations']}",
+                                        style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
                                         ),
-                                        textWidthBasis: TextWidthBasis.parent
+                                        Center(
+                                          child: Wrap(
+                                            children:[
+                                              Text("${futureDate['explanation2']}",
+                                            style: TextStyle(fontSize: 11,
+                                            ),
+                                            textWidthBasis: TextWidthBasis.parent
+                                            ),
+                                            ] 
+                                          ),
                                         ),
-                                        ] 
-                                      ),
+                                      ],
                                     ),
-                                  ],
-                                ),
-                              )
-                            ]
-                          ),
+                                  )
+                                ]
+                              ),
+                            ),
                         ),
+                      ),
+                      SizedBox(
+                        child: Container(
+                          width: 500,
+                          height: 300, 
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black87),          
+                            ),
+                        ),
+                      ),
+                      ],
                     ),
-                  ),
-                  ],
                 ),
-              ),
-              floatingActionButton: FloatingActionButton.extended(
-                onPressed: () {
-                  ();
-                },
-                label: const Text('다시 테스트 하기'),
-                backgroundColor: Colors.blue[600],
-              ),
-              floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+                ),
+            ),
+              // floatingActionButton: FloatingActionButton.extended(
+              //   onPressed: () {
+                  
+              //   },
+              //   label: const Text('다시 테스트 하기', style: TextStyle(color: Colors.yellow),),
+              //   backgroundColor: Color.fromARGB(255, 55, 55, 55),
+              // ),
+              // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
           );
         }
       },
