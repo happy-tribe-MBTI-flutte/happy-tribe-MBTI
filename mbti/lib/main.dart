e70d1b5f782a8f4aed88c014d4316f69982abda3
sungmmmm <sadlzn77@gmail.com>
2023-10-14 15:45:59 +0900

    메인페이지 뷰 수정중

--- mbti/lib/main.dart
+++ mbti/lib/main.dart
@@ -29,11 +29,25 @@ class _MainState extends State<Main> {
         width: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('page_img/m_img.png'),
+            image: AssetImage('assets/page_img/m_img.png'),
             fit: BoxFit.cover,
           ),
         ),
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          InkWell(
+            // 이미지 버튼을 추가합니다.
+            onTap: () {
+              // 이미지 버튼을 눌렀을 때 실행할 동작을 여기에 추가합니다.
+            },
+            child: Container(
+              padding: EdgeInsets.all(10), // 이미지 주변에 여백을 추가합니다.
+              child: Image.asset(
+                'assets/page_img/github_img.png', // 이미지의 경로를 지정합니다.
+                width: 50, // 이미지의 너비를 조정합니다.
+                height: 50, // 이미지의 높이를 조정합니다.
+              ),
+            ),
+          ),
           AudioWidget.assets(
             child: TextButton(
                 onPressed: () {
@@ -46,60 +60,68 @@ class _MainState extends State<Main> {
             play: _play,
           ),
           Text(
-            '나와 맞는 여행지에 어울리는 술과 노래는 뭘까?',
+            // '나와 맞는 여행지에 어울리는 술과 노래는 뭘까?',
+            '내 소울 여행지와 어울리는 술과 노래는?',
             style: TextStyle(
-                letterSpacing: 1, fontSize: 12, fontWeight: FontWeight.w700),
+                letterSpacing: 1, fontSize: 15, fontWeight: FontWeight.w700),
           ),
           SizedBox(
             height: 20,
           ),
           Text('흥의 민족',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
           SizedBox(
-            height: 20,
+            height: 10,
           ),
           Text('MBTI',
               style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
-                  color: Colors.blue)),
+                  color: Color.fromARGB(246, 235, 97, 223))),
           SizedBox(
-            height: 40,
+            height: 30,
           ),
-          SizedBox(
-            height: 50,
-            width: 250,
-            child: ElevatedButton(
-              // style: ElevatedButton.styleFrom(maximumSize: Size(100, 100)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MBTIQuestionPage()),
-                );
-              },
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      '테스트 하러가기',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-                    ),
-                    Icon(Icons.arrow_right),
-                  ],
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: SizedBox(
+              height: 50,
+              width: 250,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(247, 160, 150, 235),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MBTIQuestionPage()),
+                  );
+                },
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '테스트 하러가기',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w700),
+                      ),
+                      Icon(Icons.arrow_right),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-          Container(
-            height: 100,
-            child: ResultMusic(),
-          ),
+          // Container(
+          //   height: 100,
+          //   child: ResultMusic(),
+          // ),
           SizedBox(
             height: 50,
             width: 250,
             child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(247, 160, 150, 235)),
               // style: ElevatedButton.styleFrom(maximumSize: Size(100, 100)),
               onPressed: () {
                 Navigator.push(
@@ -111,17 +133,20 @@ class _MainState extends State<Main> {
                 );
               },
 
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      '결과 예시',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-                    ),
-                    Icon(Icons.arrow_right),
-                  ],
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '결과 예시',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w700),
+                      ),
+                      Icon(Icons.arrow_right),
+                    ],
+                  ),
                 ),
               ),
             ),
