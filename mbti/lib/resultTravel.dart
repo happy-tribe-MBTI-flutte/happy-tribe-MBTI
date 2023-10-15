44a739473a4aa8d57fc5b6513a53435b39cbc9b6
Jo Young <githuborigin@gmail.com>
2023-10-15 16:24:51 +0900

    트레블 페이지 리팩토링 모델추가 유투브페이지  제작중

--- mbti/lib/resultTravel.dart
+++ mbti/lib/resultTravel.dart
@@ -13,13 +13,11 @@ import 'package:mbti/model/mbti_travel.dart';
 class resultTravel extends StatelessWidget {
   const resultTravel({super.key, this.mbti});
   final mbti;
-  
+
   @override
   Widget build(BuildContext context) {
-    
     MbtiTravel mbtiModel;
     var selectedMBTI;
-
 
     Future MbtiJsonDecode() async {
       String data = await rootBundle.loadString('travel_img/newTravel.json');
@@ -44,81 +42,67 @@ class resultTravel extends StatelessWidget {
         } else {
           MbtiTravel futureDate = snapshot.data; // 데이터를 가져옴
           return Scaffold(
-            body: Card(
-              margin: EdgeInsets.all(30),
-              child: Center(
-                child: AnimatedContainer(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.bottomLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Color.fromARGB(255, 245, 245, 170),
-                          Colors.white
-                        ]),
-                  ),
-                  duration: Duration(seconds: 1),
-                  child: Container(
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Center(
-                            child: Container(
-                              alignment: Alignment.center,
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  // border:
-                                  //     Border(bottom: BorderSide(color: Colors.grey)),
-                                  ),
+            body: Center(
+              child: Container(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.all(30),
+                  child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: Container(
+                            child: Center(
                               child: Text(futureDate.title,
                                   style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 20)),
+                                      fontWeight: FontWeight.w400, fontSize: 20)),
                             ),
                           ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Container(
-                            height: 40,
-                            decoration: BoxDecoration(
-                                // border: Border.all(color: Colors.grey)
-                                ),
-                            child: Center(
-                                child: Text(
-                              "${mbti.toUpperCase()}",
-                              style: TextStyle(
-                                  fontSize: 35, fontWeight: FontWeight.bold),
-                            )),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Container(
-                            height: 280,
-                            decoration: BoxDecoration(
-                                border: Border.all(color: Colors.grey),
-                                color: Colors.blue),
-                            child: Column(
-                              children: [
-                                Expanded(
-                                  child: Image(
-                                    fit: BoxFit.cover,
-                                    height: 100,
-                                    width: double.infinity,
-                                    image: AssetImage(futureDate.img),
-                                  ),
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              // border: Border.all(color: Colors.grey)
+                              ),
+                          child: Center(
+                              child: Text(
+                            "${mbti.toUpperCase()}",
+                            style: TextStyle(
+                                fontSize: 35, fontWeight: FontWeight.bold),
+                          )),
+                        ),
+                        Container(
+                          height: 310,
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                              color: Colors.blue),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(futureDate.Overseas,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20)),
+                              ),
+                              Image(
+                                fit: BoxFit.cover,
+                                height: 150,
+                                width: double.infinity,
+                                image: AssetImage(futureDate.img),
+                              ),
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Expanded(
+                                    child: Column(
                                       children: [
-                                        Text('한국 ',
+                                        Text('한국',
                                             style: TextStyle(
                                                 fontSize: 18.0,
                                                 fontWeight: FontWeight.bold,
@@ -129,13 +113,13 @@ class resultTravel extends StatelessWidget {
                                                 fontWeight: FontWeight.bold)),
                                       ],
                                     ),
-                                    Image(
-                                        image:
-                                            AssetImage('page_img/airplane.png'),
-                                        fit: BoxFit.cover),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
+                                  ),
+                                  Image(
+                                      alignment: Alignment.center,
+                                      image: AssetImage('page_img/airplane.png'),
+                                      fit: BoxFit.contain),
+                                  Expanded(
+                                    child: Column(
                                       children: [
                                         Text('외국 ',
                                             style: TextStyle(
@@ -148,85 +132,77 @@ class resultTravel extends StatelessWidget {
                                                 fontWeight: FontWeight.bold)),
                                       ],
                                     ),
-                                  ],
-                                ),
-                                Divider(
-                                  height: 10,
-                                  color: Colors.white,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
+                                  ),
+                                ],
+                              ),
+                              Divider(
+                                color: Colors.white,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  height: 30,
                                   child: Text(futureDate.explanation,
+                                      textAlign: TextAlign.start,
                                       style: TextStyle(
                                           fontSize: 12.0, color: Colors.white)),
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                          Container(
-                            height: 175,
-                            decoration: BoxDecoration(
-                                border: Border.all(color: Colors.grey)),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Center(
-                                  child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      futureDate.expl,
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 4,
-                                    ),
-                                    if (futureDate.expl.length > 10)
-                                      TextButton(
-                                        child: Text(
-                                          "더보기",
-                                          overflow: TextOverflow.ellipsis,
-                                          maxLines: 5,
-                                        ),
-                                        onPressed: () {
-                                          showDialog(
-                                              context: context,
-                                              barrierDismissible:
-                                                  true, // 바깥 영역 터치시 닫을지 여부
-                                              builder: (BuildContext context) {
-                                                return AlertDialog(
-                                                  title: Center(
-                                                      child: Text(
-                                                          '${mbti.toUpperCase()}')),
-                                                  content: Container(
-                                                    height: 400,
-                                                    child:
-                                                        SingleChildScrollView(
-                                                      scrollDirection:
-                                                          Axis.vertical,
-                                                      child: Text(
-                                                          "${futureDate.expl_Add}"),
-                                                    ),
-                                                  ),
-                                                  actions: [
-                                                    TextButton(
-                                                      child: Text('확인'),
-                                                      onPressed: () {
-                                                        Navigator.of(context)
-                                                            .pop();
-                                                      },
-                                                    ),
-                                                  ],
-                                                );
-                                              });
-                                        },
-                                      )
-                                  ],
-                                ),
-                              )),
-                            ),
-                          )
-                        ]),
-                  ),
+                        ),
+                        Container(
+                          height: 150,
+                          margin: EdgeInsets.all(16),
+                          child: Center(
+                              child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                futureDate.expl,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 4,
+                              ),
+                              if (futureDate.expl.length > 10)
+                                TextButton(
+                                  child: Text(
+                                    "더보기",
+                                    maxLines: 5,
+                                  ),
+                                  onPressed: () {
+                                    showDialog(
+                                        context: context,
+                                        barrierDismissible:
+                                            true, // 바깥 영역 터치시 닫을지 여부
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title: Center(
+                                                child: Text(
+                                                    '${mbti.toUpperCase()}')),
+                                            content: Container(
+                                              height: 400,
+                                              child: SingleChildScrollView(
+                                                scrollDirection: Axis.vertical,
+                                                child: Text(
+                                                    "${futureDate.expl_Add}"),
+                                              ),
+                                            ),
+                                            actions: [
+                                              TextButton(
+                                                child: Text('확인'),
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                },
+                                              ),
+                                            ],
+                                          );
+                                        });
+                                  },
+                                )
+                            ],
+                          )),
+                        )
+                      ]),
                 ),
               ),
             ),
