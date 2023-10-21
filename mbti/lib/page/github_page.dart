757c9d3ef63a02776d3951c35e6d4ba46e0d4018
Jo Young <githuborigin@gmail.com>
2023-10-22 00:37:46 +0900

    깃허브 웹사이즈 조정 및 메인페이지 깃허브 이동  플로팅버튼 위치조정

--- mbti/lib/page/github_page.dart
+++ mbti/lib/page/github_page.dart
@@ -17,48 +17,51 @@ class GithubPage extends StatelessWidget {
         elevation: 0,
       ),
       extendBodyBehindAppBar: true,
-      body: Container(
-        decoration: BoxDecoration(
-            color: Color.fromARGB(221, 253, 166, 252),
-            image: DecorationImage(
-                image: AssetImage('page_img/m_img.png'), fit: BoxFit.cover)),
-        child: ListView(
-            padding: EdgeInsetsDirectional.symmetric(horizontal: 70),
-            children: [
-              SizedBox(height: 70),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 30, bottom: 30),
-                  child: Text("MAKERS", style: GoogleFonts.anton(fontSize: 50)),
+      body: Center(
+        child: Container(
+          width: 390,
+          height: 844,
+          decoration: BoxDecoration(
+              color: Color.fromARGB(221, 253, 166, 252),
+              image: DecorationImage(
+                  image: AssetImage('page_img/m_img.png'), fit: BoxFit.cover)),
+          child: ListView(
+              padding: EdgeInsetsDirectional.symmetric(horizontal: 70),
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30, bottom: 30),
+                    child: Text("MAKERS", style: GoogleFonts.anton(fontSize: 50)),
+                  ),
                 ),
-              ),
-              SizedBox(height: 120),
-              TextTile(
-                name: "조영",
-                git: "https://github.com/gitjoyoung",
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextTile(
-                name: "오정현",
-                git: "https://github.com/jeonghyun77",
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextTile(
-                name: "임동욱",
-                git: "https://github.com/dongwook1179",
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextTile(
-                name: "손성민",
-                git: "https://github.com/sungmmmm",
-              ),
-            ]),
+                SizedBox(height: 120),
+                TextTile(
+                  name: "조영",
+                  git: "https://github.com/gitjoyoung",
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextTile(
+                  name: "오정현",
+                  git: "https://github.gcom/jeonghyun77",
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextTile(
+                  name: "임동욱",
+                  git: "https://github.com/dongwook1179",
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextTile(
+                  name: "손성민",
+                  git: "https://github.com/sungmmmm",
+                ),
+              ]),
+        ),
       ),
     );
   }
