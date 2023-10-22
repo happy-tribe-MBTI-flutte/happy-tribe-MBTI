c79c8f304cdff3d7508937c07032e1d30f2169d2
Jo Young <githuborigin@gmail.com>
2023-10-23 07:54:45 +0900

    결과 예시 TEST 페이지 수정

--- mbti/lib/page/main_page.dart
+++ mbti/lib/page/main_page.dart
@@ -180,7 +180,7 @@ class _MainPageState extends State<MainPage> {
                             context,
                             MaterialPageRoute(
                                 builder: (context) => result(
-                                      mbtiResult: 'ENTJ',
+                                      mbtiResult: 'TEST',
                                     )),
                           );
                         },
